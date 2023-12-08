592214dd73e5b7ff7b5475ef8c1b90e5463827c6
Mhamudulislam <141499999+Mhamudulislam@users.noreply.github.com>
2023-12-08 21:19:17 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function()
+{
+    // swiper
+    const swiper = new Swiper('.swiper', {
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+    });
+
+
+    
+
+    // ps_gallery
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-36251023-1']);
+_gaq.push(['_setDomainName', 'jqueryscript.net']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+
+
+
+
+
+
+
+
+})
